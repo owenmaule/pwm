66ca9a4542f2a79259b1767d51ac45ab9cbdd662
Owen Maule <o@owen-m.com>
2015-05-05 13:43:47 +0100

    Title is a link to the domain relative app root

--- template.php
+++ template.php
@@ -60,7 +60,7 @@ if( ! ( $pathUp = $content[ 'rel_path' ] ) )
 			<img class="logo" src="<?php echo $pathUp; ?>tux-ninja.png" alt="Tux the penguin as a badass ninja" />
 		</div>
 		<div id="header-overlay">
-			<h1><span class="app-title" title="Guarding your passwords">PwN <span class="first-part">Pass</span> <span>word</span> Ninja</span></h1>
+			<h1><a href="<?php echo $pathUp; ?>"><span class="app-title" title="Guarding your passwords">PwN <span class="first-part">Pass</span> <span>word</span> Ninja</span></a></h1>
 			<h2>By <a href="http://owen-m.com/" target="_blank">Owen Maule</a></h2>
 			<nav class="pure-menu pure-menu-horizontal">
 				<ul class="pure-menu-list"><?php
