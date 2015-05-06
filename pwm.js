aebd4cb7d465cb1955a8504c037e75c51092303e
Owen Maule <o@owen-m.com>
2015-05-06 05:28:00 +0100

    Added Javascript to the project including jQuery

---
+++ pwm.js
@@ -0,0 +1,26 @@
+/*
+	pwm Password Manager
+	Copyright Owen Maule 2015
+	o@owen-m.com
+	https://github.com/owenmaule/pwm
+
+	License: GNU Affero General Public License v3
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/agpl.html>.	
+*/
+
+$( function() {
+	console.log( "pwm Password Manager (c) Copyright Owen Maule 2015 <o@owen-m.com> http://owen-m.com/" );
+	console.log( "Latest version: https://github.com/owenmaule/pwm   Licence: GNU Affero General Public License" );
+} );
